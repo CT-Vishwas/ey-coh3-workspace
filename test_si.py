797365b4e5821c89228ac0833a9167920ff4395a
Vishwas K Singh <vishwas@cloudthat.com>
2026-05-20 14:37:33 +0530

    various import ways

--- test_si.py
+++ test_si.py
@@ -1,15 +1,17 @@
 # Imports
-import simple_interestv4
-
+# import simple_interestv4
+# from simple_interestv4 import si
+from simple_interestv4 import si as s
 # Constants
-
 
 # Function Definitions
 def main():
     p = 100000
     r = 0.07
     t = 12
-    print(f"Simple Interest of {p} at {r} rate is {simple_interestv4.si(p,r,t)}")
+    # print(f"Simple Interest of {p} at {r} rate is {simple_interestv4.si(p,r,t)}")
+    # print(f"Simple Interest of {p} at {r} rate is {si(p,r,t)}")
+    print(f"Simple Interest of {p} at {r} rate is {s(p,r,t)}")
 
 # Function Calling
 if __name__ == '__main__':
