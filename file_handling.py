2c921ae2d45c84e664231efb3b7fccad27d13597
Vishwas K Singh <vishwas@cloudthat.com>
2026-05-21 14:52:40 +0530

    Oops

--- file_handling.py
+++ file_handling.py
@@ -8,8 +8,15 @@ print("This is vishwas\\t I am learning python\\n")
 # print(data)
 # fh.close()
 
-file_path = r'C:\Users\VishwasKSingh\Workspace\ey-coh3-workspace\data\data.csv'
-with open(file_path, 'r') as fh:
-    data = fh.read()
-    print(data)
-    fh.write("\nmanoj,25,tumkur")
+file_path = r'C:\Users\VishwasKSingh\Workspace\ey-coh3-workspace\data\data1.csv'
+try:
+    with open(file_path, 'r') as fh:
+        data = fh.read()
+        print(data)
+        fh.write("\nmanoj,25,tumkur")
+except FileNotFoundError:
+    print(f"File not found: {file_path}")
+except IOError as e:
+    print(f"IO error occurred: {e}")
+except Exception as e:
+    print(f"An error occurred: {e}")
